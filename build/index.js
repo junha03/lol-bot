58264b9cf2410e2a1187d74e4db10a167e927921
junha03 <angry03@uos.ac.kr>
2024-09-27 16:37:21 +0900

    add base event

--- build/index.js
+++ build/index.js
@@ -1,22 +1,19 @@
-import { Client } from "discord.js";
-import { config } from "dotenv";
-
-config();
-
-const client = new Client({
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const client = new discord_js_1.Client({
     intents: ["Guilds", "GuildMessages", "MessageContent"],
 });
-
 client.on("messageCreate", async (msg) => {
+    console.log(msg.content);
     if (msg.content == "hello") {
         msg.reply("hi");
     }
 });
-
 async function start() {
     await client.login(process.env.DISCORD_BOT_KEY);
-
     console.log("login success");
 }
-
 start();
